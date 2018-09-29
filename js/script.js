414cbb95b533766553f0f740815cd1244fbd9472
Erik <eriklopezramirez@Eriks-MacBook-Pro.local>
2018-09-28 22:17:49 -0400

    Refactoring

--- js/script.js
+++ js/script.js
@@ -7,9 +7,12 @@ FSJS project 2 - List Filter and Pagination
 const list = document.querySelectorAll('.student-item');
 let pagesNumber;
 let allStudents;
-let htmlDiv;
-let selection;
+let pageDiv;
+
 let unorderedL;
+let lI;
+let a;
+
 
 // Create a function to hide all of the items in the list except for the ten you want to show
 // Tip: Keep in mind that with a list of 54 students, the last page will only display four
@@ -27,22 +30,25 @@ const  ShowPage = (list,page) => {
 
 // Create and append the pagination links - Creating a function that can do this is a good approach
 const appendPageLinks = (list) => {
-    // I dont understand this instruction: (if pagination already exists, remove it), Am I right to assume that it has to do with node.removeChild();????
+    /* determine how many pages are needed for the list by dividing the total number
+of list items by the max number of items per page*/
     pagesNumber = Math.floor(list.length/10);
     allStudents = list.length;
-    ////I am unsure if I did some of these steps right:  create a d​iv​, give it the “pagination” class, and append it to the .page div // add a ul  to the “pagination” div
-// f​or​ every page
-    selection = document.querySelector('.page');
-    htmlDiv = document.createElement('div');
-    htmlDiv.className = 'pagination';
-    selection.appendChild('htmlDiv');
+
+// create a d​iv​, give it the “pagination” class, and append it to the .page div
+      
+    pageDiv = document.createElement('div');
+    pageDiv.className = 'pagination';
+    pageDiv.appendTo('.page');
+    pageDiv.append(ul);
     
-
-    for (let i = 0; i < pagesNumber.length; i++) {
-        unorderedL = document.createElement('ul');
-    }
-
-
+    
+    
+    
+    unorderedL = document.createElement('ul');
+    lI = document.createElement('li');
+    a = document.createElement('a');
+}
 
 // Add functionality to the pagination buttons so that they show and hide the correct items
 // Tip: If you created a function above to show/hide list items, it could be helpful here
