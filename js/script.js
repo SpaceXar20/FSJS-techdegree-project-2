0bde8c9e7f484a27c14ef180b050ebd5a9b06963
Erik <eriklopezramirez@Eriks-MacBook-Pro.local>
2018-09-29 21:25:38 -0400

    looped over pagination links

--- js/script.js
+++ js/script.js
@@ -10,7 +10,7 @@ let allStudents;
 let pageDiv;
 
 let unorderedL;
-let lI;
+let li; 
 let a;
 
 
@@ -49,7 +49,7 @@ of list items by the max number of items per page*/
 // add l​i​ and ​a​ tags with the page number text
     for (let i = 0; i < pagesNumber.length; i++) {
         
-    lI = document.createElement('li');
+    li = document.createElement('li');
     a = document.createElement('a');
     
     /*
@@ -62,8 +62,18 @@ and use event delegation to target the a tags to define what happens they are cl
         ShowPage();
 
      // loop over pagination links to remove active class from all
-      for (let i= 0; i < appendPageLinks.length; i++) {
+      for (let i= 0; i < pagesNumber.length; i++) {
+        const listItems = document.createElement('li');
+        ul.appendChild(listItems);
+        const a = document.createElement('a');
+        a.textContent = i + 1;
+        list.appendChild(a);
         
+        if  (a.textContent=== i) {
+            let pageButtons = document.createElement('button');
+            let pageLinks = pageButtons * pagesNumber;
+            $(ul).append(pageLinks); 
+        }
      
      /* add the active class to the link that was just clicked, otherwise known as the
 event.target*/
@@ -72,7 +82,7 @@ event.target*/
     }
     
 }
-
+appendPageLinks(list);
 // Add functionality to the pagination buttons so that they show and hide the correct items
 // Tip: If you created a function above to show/hide list items, it could be helpful here
 
