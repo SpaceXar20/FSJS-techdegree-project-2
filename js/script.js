b1d73955e858e204430e6d11c163c27affaaf134
Erik <eriklopezramirez@Eriks-MacBook-Pro.local>
2018-10-01 17:12:50 -0400

    Project Refactored

--- js/script.js
+++ js/script.js
@@ -3,91 +3,71 @@ Treehouse Techdegree:
 FSJS project 2 - List Filter and Pagination
 ******************************************/
 
-// Add variables that store DOM elements you will need to reference and/or manipulate
-const list = document.querySelectorAll('.student-item');
-let pagesNumber;
-let allStudents;
-let pageDiv;
+// variables for project
+const list = document.querySelector('ul');
+const numberOfStudents = list.children;
+const studentsAnyNumber = 10;
+const selectSearchBox = document.querySelector('.student-search');
+const buttonDiv = document.querySelector('.pagination');
+const buttonUl = buttonDiv.querySelector('ul');
+const zeroResults = document.querySelector('.no-result');
 
-let unorderedL;
-let li; 
-let a;
-
-
-// Create a function to hide all of the items in the list except for the ten you want to show
-// Tip: Keep in mind that with a list of 54 students, the last page will only display four
-const  ShowPage = (list,page) => {
-    for (let i = 0; i < list.length; i++) {
-     if (i  >= (page * 10) - 10 && i < (page * 10)) {
-        list[i].style.display = 'block';
-    } else {
-        list[i].style.display = 'none';
-        }
-    }
-};    
-
-
-
-// Create and append the pagination links - Creating a function that can do this is a good approach
-const appendPageLinks = (list) => {
-    /* determine how many pages are needed for the list by dividing the total number
-of list items by the max number of items per page*/
-    pagesNumber = Math.floor(list.length/10);
-    allStudents = list.length;
-
-// create a d​iv​, give it the “pagination” class, and append it to the .page div
-      
-    pageDiv = document.createElement('div');
-    pageDiv.className = 'pagination';
-    pageDiv.appendTo('.page');
-    // add a ul to the “pagination” div
-    unorderedL = document.createElement('ul');
-    pageDiv.append(ul);
-    
-    
-    // f​or​ every page
-// add l​i​ and ​a​ tags with the page number text
-    for (let i = 0; i < pagesNumber.length; i++) {
-        
-    li = document.createElement('li');
-    a = document.createElement('a');
-    
-    /*
-    add an event listener to each a​ ​ tag, or add an event listener to the pagination div,
-and use event delegation to target the a tags to define what happens they are clicked
-    */
-    document.querySelector('pageDiv').addEventListener('click')
-
-     // calls the showPage function to display the appropriate page
-        ShowPage();
-
-     // loop over pagination links to remove active class from all
-      for (let i= 0; i < pagesNumber.length; i++) {
-        const listItems = document.createElement('li');
-        ul.appendChild(listItems);
-        const a = document.createElement('a');
-        a.textContent = i + 1;
-        list.appendChild(a);
-        
-        if  (a.textContent=== i) {
-            let pageButtons = document.createElement('button');
-            let pageLinks = pageButtons * pagesNumber;
-            $(ul).append(pageLinks); 
-        }
-     
-     /* add the active class to the link that was just clicked, otherwise known as the
-event.target*/
-          
-      }  
-    }
-    
+// this function displays the amount of pages based on how many students there are in total
+const totalNumberOfPages = () => {
+    let pageNumbers = Math.floor(numberOfStudents.length /studentsAnyNumber);
+    return pageNumbers;
 }
-appendPageLinks(list);
-// Add functionality to the pagination buttons so that they show and hide the correct items
-// Tip: If you created a function above to show/hide list items, it could be helpful here
 
+//This functions makes it so that 10 students are displayed
+const tenStudents = () => {
+    for (let i = 0; i < numberOfStudents.length; i++) {
+        if (i < studentsAnyNumber) {
+           numberOfStudents[i].style.display= '';     
+        } else {
+            numberOfStudents[i].style.display= 'none';  
+        }
+        
+    }
+}
 
+//This functions displays the box to search for students
+let searchBox = document.createElement('input');
+let button = document.createElement('button');
+const showSearchBox = () => {
+    searchBox.placeholder = 'Search for students...';
+    button.textContent = 'Search';
+    selectSearchBox.appendChild(searchBox);
+    selectSearchBox.appendChild(button);
+}  
 
+//this (for) loop divides students pages
+for (let i = 1; i <= totalNumberOfPages.length; i++) {
+  let li = document.createElement('li');
+  let pageLinks = document.createElement('a');
+    li.className = 'active';
+    pageLinks.href = '#';
+    pageLinks.textContent = i;
+    buttonUl.appendChild(li);
+    li.appendChild(pageLinks);
+}
 
+buttonDiv.addEventListener('click',(event) => {
+    zeroResults.innerHTML = '';
+    let buttonsNumber = parseInt(event.target.textContent);
+    let maximum = buttonsNumber * 10;
+    let minimum = max - 10;
+    for (let i = 0; i < numberOfStudents.length; i++) {
+       if (i >= minimum && i < maximum) {
+           numberOfStudents[i].style.display = '';
+       }    else {
+           numberOfStudents[i].style.display = 'none';
+       }     
+        
+    }
+});
 
+//Shows 10 students when the browser loads
+totalNumberOfPages();
 
+//This function displays the search box
+showSearchBox();
